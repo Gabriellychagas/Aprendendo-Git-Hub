0fcd14b4f645da7e21b871db3069233156133b5c
Gabrielly Dos Santos Chagas <ht1810847@HTO.IFSP.EDU.BR>
2019-08-05 10:02:59 -0300

    Estou subindo o arquivo de um menu em php

---
+++ Menu.php
@@ -0,0 +1,8 @@
+<html>
+<?php
+
+	echo "<marquee><h1>Menu Matematico</marquee><p>";
+	echo "<a href = 'hto.ifsp.edu.br'>Site Hortolandia</a><p>";
+	echo "<a href = 'www.ifsp.edu.br'> Site Reitoria</a>";
+?>	
+</html>

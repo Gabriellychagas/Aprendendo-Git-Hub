a34a671242d3fd90fc157552c8cfd262829ee320
Gabrielly Dos Santos Chagas <ht1810847@hto.ifsp.edu.br>
2019-08-06 09:32:15 -0300

    Alterei depois de Reitoria colocando o nome da escola

--- Menu_matematico.php
+++ Menu_matematico.php
@@ -3,6 +3,6 @@
 
 	echo "<marquee><h1>Menu Matematico</marquee><p>";
 	echo "<a href = 'hto.ifsp.edu.br'>Site Hortolandia</a><p>";
-	echo "<a href = 'www.ifsp.edu.br'> Site Reitoria</a>";
+	echo "<a href = 'www.ifsp.edu.br'> Site Reitoria do IFSP- Câmpus Hortolandia</a>";
 ?>	
 </html>
